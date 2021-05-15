02c3b4fa0175e5fc9cf6ec1e2c10db018a258813
Aaron-labo <wxy1294685672@gamil.com>
2021-05-15 14:42:27 +0800

    更新蜂鸣器相关代码

--- 02.BEEP/Core/Inc/beep.h
+++ 02.BEEP/Core/Inc/beep.h
@@ -9,8 +9,10 @@
 #define INC_BEEP_H_
 
 #include "gpio.h"
+//蜂鸣器宏定义
 #define BEEP_On() HAL_GPIO_WritePin(BEEP_GPIO_Port, BEEP_Pin, GPIO_PIN_SET)
 #define BEEP_Off() HAL_GPIO_WritePin(BEEP_GPIO_Port, BEEP_Pin, GPIO_PIN_RESET)
 #define BEEP_Tog() HAL_GPIO_TogglePin(BEEP_GPIO_Port, BEEP_Pin)
+#define BEEP HAL_GPIO_ReadPin(BEEP_GPIO_Port, BEEP_Pin)
 
 #endif /* INC_BEEP_H_ */
